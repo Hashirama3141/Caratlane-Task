d030cca752acfa524249dbd6b03802768d5dd1b4
Ramkumar <ramkumarcrmd@gmail.com>
2017-01-16 07:55:34 +0530

    Added dummy styles

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = new express();
 
+app.use(express.static('public'));
 
 app.get('/', function (request, response) {
     response.send('public/index.html');
